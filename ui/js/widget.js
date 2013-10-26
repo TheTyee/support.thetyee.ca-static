a280720dcd9b549df9e2791abc305ed557d3db09
Phillip Smith <ps@phillipadsmith.com>
2013-10-25 19:03:50 -0500

    Whoops. SSL!

--- ui/js/widget.js
+++ ui/js/widget.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $.getJSON("http://widgets.thetyee.ca/progress.json?cb=?&campaign=national&date_end=2013-11-18&goal=100000&date_start=2013-10-25", function(data){
+    $.getJSON("https://widgets.thetyee.ca/progress.json?cb=?&campaign=national&date_end=2013-11-18&goal=100000&date_start=2013-10-25", function(data){
         var result = data.result;
         $(".goal").text( result.goal_formatted );
         $(".percentage").text( result.percentage );
@@ -29,7 +29,7 @@ $(document).ready(function(){
     });
     var progress = setInterval(function(){
         /* query the completion percentage from the server */
-        $.getJSON("http://widgets.thetyee.ca/progress.json?cb=?&campaign=national&date_end=2013-11-18&goal=100000&date_start=2013-10-25", function(data){
+        $.getJSON("https://widgets.thetyee.ca/progress.json?cb=?&campaign=national&date_end=2013-11-18&goal=100000&date_start=2013-10-25", function(data){
             var result = data.result;
             $(".percentage").html( result.percentage );
             $(".count").html( result.people );
