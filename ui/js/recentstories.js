27176d5c571ac7e0d9f06987f05530ca94d1c6f8
Ashley Robinson <ashley@reassemble.ca>
2015-01-22 22:53:45 -0700

    Getting rid of weird characters

--- ui/js/recentstories.js
+++ ui/js/recentstories.js
@@ -1,35 +1,33 @@
 $(function() {
-  var tyeeAPI = "http://api.thetyee.ca/v1/latest/4",
-      recentStoryHead = '<h3 class="recent-stories">Recent Stories by The Tyee</h3>',
-      tyeeStories = [recentStoryHead];
+    var tyeeAPI = "http://api.thetyee.ca/v1/latest/4",
+        recentStoryHead = '<h3 class="recent-stories">Recent Stories by The Tyee</h3>',
+        tyeeStories = [recentStoryHead];
 
-  $.ajax({
-     type: "GET",
-     url: tyeeAPI,
-     async: true,
-     dataType: "jsonp",
-     success: function(data){
-        addStories(data.hits.hits);
-    
-   }
-  });
+    $.ajax({
+        type: "GET",
+        url: tyeeAPI,
+        async: true,
+        dataType: "jsonp",
+        success: function(data) {
+            addStories(data.hits.hits); 
+        }
+    });
 
 
-  function addStories(data){
+    function addStories(data) {
         $.each(data, function(index, story) {
-          var article = story._source,
-              url = '<a href="'+ article.uri + '">',
-              title = '<h5 class="story-title">' + article.title + '</h5>',
-              tease = '<p class="story-tease">' + article.teaser + '</p>',
-              list;
+            var article = story._source,
+                url = '<a href="' + article.uri + '">',
+                title = '<h5 class="story-title">' + article.title + '</h5>',
+                list;
 
-          list = url + title + '</a>';
-          tyeeStories.push(list);
+            list = url + title + '</a>';
+            tyeeStories.push(list);
 
-            
-           });
 
-      $('.recent-stories').html(tyeeStories.join(''));
-    
-  }
+        });
+
+        $('.recent-stories').html(tyeeStories.join(''));
+
+    }
 });
